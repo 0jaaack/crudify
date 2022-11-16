6a4d00001a82821ff71b36bb6f774b365ba4a423
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2022-11-17 03:41:42 +0900

    [2022.11.17] Refactor: command service 추가 - 기존에는 command 관련 파일 별도로 작성하여 사용하였습니다. - 같은 관심사의 코드를 처리하기 위해 command serice 파일을 생성하였습니다.

---
+++ create-crudify-app/services/command.js
@@ -0,0 +1,44 @@
+const spawn = require("child_process").spawn;
+const ora = require("ora");
+
+class CommandService {
+  constructor(project) {
+    this.project = project;
+  }
+
+  async installPackage () {
+    const spinner = ora("패키지 파일을 설치 중이에요!").start();
+
+    const installRunner = () => {
+      return new Promise((resolve, reject) => {
+        const process = spawn("npm", ["install", "--production", "--no-optional"], {
+          cwd: this.project.path
+        });
+        process.on("close", resolve);
+        process.on("error", reject);
+      });
+    }
+
+    try {
+      await installRunner();
+      spinner.succeed("프로젝트의 설치가 성공적으로 완료되었습니다.");
+    } catch {
+      spinner.fail("프로젝트를 설치하는 중 오류가 발생하였습니다.");
+      throw new Error("패키지를 설치하는 동안 에러가 발생하였습니다.");
+    }
+  };
+
+  async startCrudifyApp() {
+    console.log("crudify 서버를 실행하고, 관리자를 위한 대시보드 페이지와 연결할게요.");
+
+    const process = spawn("crudify", ["start"], {
+      cwd: this.project.path
+    });
+
+    process.on("error", () => {
+      throw new Error("서버를 실행하려는 중, 에러가 발생하였습니다.");
+    });
+  }
+}
+
+module.exports = CommandService;
